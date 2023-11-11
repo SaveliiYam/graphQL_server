fed26b48ddf464f461f1f45529fc92bc1182362f
SaveliiYam <savelkayamil5@gmail.com>
2023-11-11 15:44:29 +0300

    create simple server

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
+)
+
+func main() {
+	schema, err := graphql.NewSchema(defineSchema())
+	if err != nil {
+		log.Panic("Error when creating the graphQL schema", err)
+	}
+
+	h := handler.New(&handler.Config{
+		Schema:     &schema,
+		Pretty:     true,
+		GraphiQL:   false,
+		Playground: true,
+	})
+
+	http.Handle("/graphql", h)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Panic("Error when starting the http server", err)
+	}
+	log.Print("Server is started")
+}
